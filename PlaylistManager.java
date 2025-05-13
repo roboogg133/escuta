479c7d14cc61eedd281c8e5e6300174670b52f7a
Lima <felipebr4095@gmail.com>
2025-05-13 07:12:54 -0300

    update PlaylistManager.java

--- PlaylistManager.java
+++ PlaylistManager.java
@@ -10,29 +10,8 @@ public class PlaylistManager {
 
         if (command.equals("")) { return; } 
         else if (command.equals("new")) { if (argument.equals("")) { return; } else { JSONObject obj = new JSONObject(); obj.put("playlist-name", argument); Files.write(Paths.get("playlist-" + argument + ".json"), obj.toString(2).getBytes()); } }
-        else if (command.equals("add")) { if (argument.equals("")) { return; } else { 
-                if (args.length < 3) { System.out.println("missed!"); return; }
-                
-                String playlistName = args[1], songName = args[2], filePath = args[3];
-
-                String playlistFile = "playlist-" + playlistName + ".json";
-                String content = new String(Files.readAllBytes(Paths.get(playlistFile)));
-                JSONObject playlist = new JSONObject(content);
-
-                if (!playlist.has("songs")) { playlist.put("songs", new JSONObject()); }
-
-                JSONObject songs = playlist.getJSONObject("songs");
-
-                int songNumber = songs.length() + 1;
-                JSONObject song = new JSONObject();
-                song.put("songname", songName);
-                song.put("filename", filePath);
-
-                songs.put(String.valueOf(songNumber), song);
-
-                Files.write(Paths.get(playlistFile), playlist.toString(2).getBytes());
-            }
-        }
+        else if (command.equals("add")) { if (argument.equals("")) { return; } else { if (args.length < 3) { System.out.println("missed!"); return; } String playlistName = args[1], songName = args[2], filePath = args[3]; String playlistFile = "playlist-" + playlistName + ".json"; String content = new String(Files.readAllBytes(Paths.get(playlistFile))); JSONObject playlist = new JSONObject(content); if (!playlist.has("songs")) { playlist.put("songs", new JSONObject()); } JSONObject songs = playlist.getJSONObject("songs"); int songNumber = songs.length() + 1; JSONObject song = new JSONObject(); song.put("songname", songName); song.put("filename", filePath); songs.put(String.valueOf(songNumber), song); Files.write(Paths.get(playlistFile), playlist.toString(2).getBytes()); } }
+        
         else { System.out.println("PlaylistManager: " + command + ": not found"); }
     }
 }
