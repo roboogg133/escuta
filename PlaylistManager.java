2a1e1c84489e7f50fee45c611e4ee712966e6c94
Mr Lima <felipebr4095@gmail.com>
2025-05-12 21:31:44 -0300

    [ MrLima ] - Creating 'PlaylistManager.java' - Template to Playlist in JSON format

---
+++ PlaylistManager.java
@@ -0,0 +1,12 @@
+/* 
+[
+	"1": {
+		"songname": "Track-1",
+		"songlocal": "local/ online",
+		"link": "file:///storage/emulated/0/Escuta-Songs/Track-1.mp3",
+		"link": "https://yt.com/wacth?Track-1.mp3"
+	}
+	
+]
+*/
+
